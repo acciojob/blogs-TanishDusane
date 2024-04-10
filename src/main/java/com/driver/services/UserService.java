4c87d81f5e4bd67df30afbef72f5d5cff1425a6b
Tanish Dusane <91111180+DeathxHunter@users.noreply.github.com>
2024-04-11 01:42:06 +0530

    Added return type

--- src/main/java/com/driver/services/UserService.java
+++ src/main/java/com/driver/services/UserService.java
@@ -1,5 +1,6 @@
 package com.driver.services;
 
+import com.driver.models.Blog;
 import com.driver.models.User;
 import com.driver.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +14,26 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public void createUser(String username, String password){
+    public User createUser(String username, String password){
         User user = new User(username,password);
         user.setUsername(username);
         user.setPassword(password);
-        userRepository.save(user);
+         userRepository.save(user);
+
+         return user;
     }
 
     public void deleteUser(int userId){
         userRepository.deleteById(userId);
     }
 
-    public void updateUser(Integer id, String password){
+    public User updateUser(Integer id, String password){
         Optional<User> userOptional = userRepository.findById(id);
         if (userOptional.isPresent()){
             User user = userOptional.get();
             user.setPassword(password);
-            userRepository.save(user); // Removed return type
+            return userRepository.save(user);
         }
+        return null;
     }
 }
